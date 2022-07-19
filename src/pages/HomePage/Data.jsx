c01695099f1dd1d355a60cffcada85242db2124b
Mirziyod Sunatillayev <mirziyodsunatillayev1612@gmail.com>
2022-07-19 16:16:22 +0300

    finished navbar and Info section

---
+++ src/pages/HomePage/Data.jsx
@@ -0,0 +1,64 @@
+export const homeObjOne = {
+    lightBg: false,
+    primary: true,
+    imgStart: "", 
+    lightTopLine: true, 
+    lightText: true, 
+    lightTextDesc: true, 
+    buttonLabel: "Get Started", 
+    description: 
+    "We help owners increase their revenue. Our team of unique specialist can help you acheive your business goals. ",
+    headline: "Lead Generation Specialist for Online Business",
+    topLine: "Mirziyod Sunatillayev",
+    img: require('../../images/biznes-idea.jpg'),
+    alt: "ImageHeader",
+    start: ""
+}
+export const homeObjTwo = {
+    lightBg: false,
+    primary: true,
+    imgStart: "", 
+    lightTopLine: true, 
+    lightText: true, 
+    lightTextDesc: true, 
+    buttonLabel: "Get Started", 
+    description: 
+    "We help owners increase their revenue. Our team of unique specialist can help you acheive your business goals. ",
+    headline: "Lead Generation Specialist for Online Business",
+    topLine: "Mirziyod Sunatillayev",
+    img: require('../../images/biznes-idea1.png'),
+    alt: "ImageHeader",
+    start: ""
+}
+export const homeObjThree = {
+    lightBg: false,
+    primary: true,
+    imgStart: "", 
+    lightTopLine: true, 
+    lightText: true, 
+    lightTextDesc: true, 
+    buttonLabel: "Get Started", 
+    description: 
+    "We help owners increase their revenue. Our team of unique specialist can help you acheive your business goals. ",
+    headline: "Lead Generation Specialist for Online Business",
+    topLine: "Mirziyod Sunatillayev",
+    img: require('../../images/profile.jpg'),
+    alt: "ImageHeader",
+    start: ""
+}
+export const homeObjFour = {
+    lightBg: false,
+    primary: true,
+    imgStart: "", 
+    lightTopLine: true, 
+    lightText: true, 
+    lightTextDesc: true, 
+    buttonLabel: "Get Started", 
+    description: 
+    "We help owners increase their revenue. Our team of unique specialist can help you acheive your business goals. ",
+    headline: "Lead Generation Specialist for Online Business",
+    topLine: "Mirziyod Sunatillayev",
+    img: require('../../images/start-profil.jpg'),
+    alt: "ImageHeader",
+    start: ""
+}
